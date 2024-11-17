4de1b730a13b8cab6ec8edcbb7a8d0d4676095e9
Vasant18 <vasantspujar018@gmail.com>
2024-11-17 22:43:39 +0530

    feat(api/signup.ts): implement signup API route for admin creation with JWT token generation

---
+++ apps/client/src/pages/api/signup.ts
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ensureDbConnected } from '@/lib/dbConnect';
+import { Admin } from "db";
+import jwt from "jsonwebtoken";
+import type { NextApiRequest, NextApiResponse } from 'next';
+const SECRET = "SECRET";
+
+type Data = {
+  token?: string;
+  message?: string;
+  name?: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+    console.log("handler called");
+    await ensureDbConnected()
+    const { username, password } = req.body;
+    const admin = await Admin.findOne({ username });
+    if (admin) {
+        res.status(403).json({ message: 'Admin already exists' });
+    } else {
+        const obj = { username: username, password: password };
+        const newAdmin = new Admin(obj);
+        newAdmin.save();
+
+        const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
+        res.json({ message: 'Admin created successfully', token });
+    }    
+}
