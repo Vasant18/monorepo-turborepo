e23a8843ff277a846396cd78e473a59d28d8895d
Vasant18 <vasantspujar018@gmail.com>
2024-11-17 22:39:03 +0530

    feat(next.config.mjs): update configuration to include transpilePackages for improved module handling

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -11,14 +11,29 @@
 
 // export default nextConfig;
 
+// // next.config.mjs
+
+// import withTM from 'next-transpile-modules';
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = withTM(['@repo/ui'])({
+//   reactStrictMode: true,
+//   // Other Next.js configurations
+// });
+
+// export default nextConfig;
+
+
+
+
+// next.config.mjs
 // next.config.mjs
 
-import withTM from 'next-transpile-modules';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withTM(['@repo/ui'])({
+const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@repo/ui', 'db'],
   // Other Next.js configurations
-});
+};
 
 export default nextConfig;
